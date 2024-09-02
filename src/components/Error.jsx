a47ff24a25cf2e92d9e3f928e49e2c95e6be1870
Paulo <paulopocas20@gmail.com>
2024-09-02 13:37:50 -0300

    adicionando pagina error

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+const Error=()=>{
+    return(
+        <>
+        <div>
+        <h1>Erro 404</h1>
+        <h2>Pagina não encontrada</h2>
+        </div>
+        </>
+    )
+}
+export default Error
