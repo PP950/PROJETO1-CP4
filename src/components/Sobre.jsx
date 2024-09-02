7de3f5bf7f57fa859afef9c31075aa682b34a57c
Paulo <paulopocas20@gmail.com>
2024-09-02 13:32:23 -0300

    adicionando conteudo para a pagina sobre

---
+++ src/components/Sobre.jsx
@@ -0,0 +1,14 @@
+const Sobre=()=>{
+    return(
+        <>
+        <div>
+            <h1>Sobre nos</h1>
+            <img src='src/assets/img1.jpg' alt='Sobre nos'/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Perspiciatis adipisci expedita quod quidem neque architecto eum
+        dolores explicabo rem earum in natus laudantium eligendi dignissimos,
+        officiis veniam, velit sed itaque.</p>
+        </div>
+        </>
+    )
+}
