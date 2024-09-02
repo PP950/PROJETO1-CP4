eef315b2d54ca234f819d09472a8aaf948980f73
Paulo <paulopocas20@gmail.com>
2024-09-02 13:28:44 -0300

    adicionando conteudo no home

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+const Home=()=>{
+    return(
+        <>
+    <div>
+        <h1>Home</h1>
+        <h2>Olá bem vindo ao nosso site!</h2>
+       
+    </div>
+    
+        </>
+    )
+}
+export default Home
