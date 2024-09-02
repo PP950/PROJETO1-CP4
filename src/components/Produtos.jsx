6e4732b7497984f2897136e64e392e20d0947241
Paulo <paulopocas20@gmail.com>
2024-09-02 13:39:11 -0300

    adicionando cards para os produtos

---
+++ src/components/Produtos.jsx
@@ -0,0 +1,16 @@
+const Produto=()=>{
+    return(
+        <>
+        <div>
+            <h1>Produtos</h1>
+            <div className='cards'>
+                <div className='card'>Produto 1</div>
+                <div className='card'>Produto 2</div>
+                <div className='card'>Produto 3</div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Produto
+
