f01ca4f31311abd78de56afffed6d37dfae8b273
Paulo <paulopocas20@gmail.com>
2024-09-02 13:26:47 -0300

    importando os componentes para o app e adicionando imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import Home from './components/Home'
+import {BrowserRouter as Router, Routes, Route} from 'react-route-dom'
+import House from './components/Home'
+import Produtos from './components/Produtos'
+import Sobre from './components/Sobre'
+import Error from './components/Error'
+import Contato from './components/Contato'
 import Footer from './components/Footer'
 import Nav from './components/Nav'
 function App() {
@@ -7,6 +12,7 @@ function App() {
   return (
     <>
     <Nav/>
+
 
 
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,15 @@
+import {Link} from 'react-router-dom'
 const Nav=()=>{
     return(
         <>
-        <Link to='/'>Home</Link>
+    <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/Produto'>Produto</Link></li>
+        <li><Link to='/Sobre'>Sobre</Link></li>
+    </ul>
         </>
+    
 
     )
 }
+export default Nav
