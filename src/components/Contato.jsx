946f2b85820d73fe60bb252be2da7ce86b9627cd
Paulo <paulopocas20@gmail.com>
2024-09-02 13:30:22 -0300

    adicionando conteudo em contato

---
+++ src/components/Contato.jsx
@@ -0,0 +1,13 @@
+
+const Contato=()=>{
+    return(
+        <>
+        <div>
+            <h1>Contato</h1>
+            <h2>Caso queira se comunicar conosco envie um email ou entre em contato pelo nosso numero 555-555-555</h2>
+        </div>
+        </>
+    )
+}
+export default Contato
+
