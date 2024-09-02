4ca86894851059b17ef379f0a1511ace5f69040c
Paulo <paulopocas20@gmail.com>
2024-09-02 12:43:01 -0300

    Configurando React

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Home from './components/Home'
+import Footer from './components/Footer'
+import Nav from './components/Nav'
+function App() {
+ 
+
+  return (
+    <>
+    <Nav/>
+
+
+
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+const Nav=()=>{
+    return(
+        <>
+        <Link to='/'>Home</Link>
+        </>
+
+    )
+}
